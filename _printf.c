3f5c04dd03567f45aa4242ca6e96618df4ccdc29
hmeyd <yyahmedsalm@gmail.com>
2024-11-28 17:40:31 +0100

    projet modif

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
-#include "main.h" /* Inclut le fichier d'en-tête principal*/
-#include <stdio.h>
+#include "main.h"
 /**
  * _printf - Print arguments according to a format.
  * @format: Format string containing the format specifiers.
@@ -7,87 +6,23 @@
  */
 int _printf(const char *format, ...)
 {
-	int num, mul = 1, de_d, j = 1;
-	float num2, de_p;
+	unsigned int count = 0;
 	va_list args; /* Déclare une liste pour les arguments variadiques*/
-	int count = 0; /* Initialise le compteur de caractères imprimés à 0*/
 	const char *ptr; /* Déclare un pointeur pour parcourir la chaîne format*/
-	va_start(args, format); /* Initialise la liste d'arguments variadiques*/
 	for (ptr = format; (*ptr != '\0'); ptr++) /* Parcourt chaque caractère de la chaîne format*/
 	{
 		if (*ptr == '%') /* Vérifie si le caractère actuel est '%'*/
 		{
+			if (*ptr == '%')
+			{
+				_putchar('%');
+			}
+			else
+			{
+			print_t(ptr);
+			count ++;
+			}
 			ptr++; /*Passe au caractère suivant*/
-			if (*ptr == 'c') /* Vérifie si le spécificateur est 'c'*/
-			{
-				_putchar(va_arg(args, int)); /* Imprime le caractère passé en argument*/
-				count++; /*Incrémente le compteur*/
-			}
-			else if (*ptr == 's') /* Vérifie si le spécificateur est 's'*/
-			{
-				char *str = va_arg(args, char *); /*Récupère la chaîne de caractères passée en argument*/
-				while (*str) /* Parcourt chaque caractère de la chaîne*/
-				{
-					_putchar(*str); /*Imprime chaque caractère de la chaîne*/
-					str++; /*Passe au caractère suivant de la chaîne*/
-					count++; /* Incrémente le compteur*/
-				}
-			}
-			else if (*ptr == '%') /*Vérifie si le spécificateur est '%'*/
-			{
-				_putchar('%'); /* Imprime le caractère '%'*/
-				count++; /*Incrémente le compteur*/
-			}
-			else if (*ptr == 'i')
-			{
-				num = va_arg(args, int);
-				if (num < 0)
-				{
-					_putchar('-');
-					num = -1 * num;
-				}
-				while (num / mul > 9)
-				{
-					mul = mul * 10;
-				}
-				while (mul != 1)
-				{
-					_putchar ((num / mul) % 10 + '0');
-					mul = mul / 10;
-				}
-				_putchar(num % 10 + '0');
-				count++ ;
-			}
-			else if (*ptr == 'd')
-			{
-				num2 = va_arg(args, double);
-				mul = 1;
-				while ((int)num2 / mul > 9)
-				{
-					mul = mul * 10;
-				}
-				while (mul != 1)
-				{
-					_putchar (((int)num2 / mul) % 10 + '0');
-					mul = mul / 10;
-				}
-				_putchar((int)num2 % 10 + '0');
-				/*Afficher le point décimal*/
-				if ((num2 - (int)num2) != 0)
-				{
-					_putchar('.');
-					de_p = num2 - (int)num2;
-					while (j < 4)
-					{
-						de_p = 10 * de_p;
-						de_d = (int)de_p;
-						_putchar(de_d + '0');
-						de_p = de_p - de_d;
-						j++;
-					}
-				}
-				count++;
-			}
 		}
 		else
 		{
--- main.h
+++ main.h
@@ -6,16 +6,26 @@ char _putchar (char c);
 int _printf(const char *format, ...);
 typedef struct what_format
 {
-char *type;
-void (*f)(char *separator, va_list args);
+	char *type;
+	void (*f)(va_list args);
 }what_format;
-int	sum_them_all(const unsigned int n, ...);
-void	print_numbers(const char *separator, const unsigned int n, ...);
-void	print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-void print_char(char *separator, va_list args);
-void print_integer(char *separator, va_list args);
-void print_porcentage(char *separator, va_list args);
-void print_char_ptr(char *separator, va_list args);
+void print_t(const char * const format, ...);
+void print_char(va_list args);
+void print_integer(va_list args);
+void print_porcentage(va_list args);
+void print_char_ptr(va_list args);
+void print_double(va_list args);
+void print_char(va_list args)
+{
+	_putchar(va_arg(args, int)); /* Imprime le caractère passé en argument*/
+}
+void print_char_ptr(va_list args)
+{
+	char *str = va_arg(args, char *); /*Récupère la chaîne de caractères passée en argument*/
+	while (*str) /* Parcourt chaque caractère de la chaîne*/
+	{
+		_putchar(*str); /*Imprime chaque caractère de la chaîne*/
+		str++; /*Passe au caractère suivant de la chaîne*/
+	}
+}
 #endif
