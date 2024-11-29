b68a83bb8ca34b79ba6c11796b41b7a4f817d4d2
Myriam <myriiamwayne@gmail.com>
2024-11-29 14:12:17 +0100

    modif

--- main.h
+++ main.h
@@ -4,14 +4,9 @@
 #include <stdarg.h>
 char _putchar (char c);
 int _printf(const char *format, ...);
-typedef struct what_format
-{
-	char *type;
-	void (*f)(va_list args);
-}what_format;
 void print_t(const char *format, ...);
 void print_char(va_list args);
 void print_integer(va_list args);
-void print_char_ptr(va_list args);
+int print_char_ptr(va_list args);
 void print_double(va_list args);
 #endif
