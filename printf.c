ef9c8d3c60410d997b66969a6682572080dfc000
Myriam <myriiamwayne@gmail.com>
2024-11-29 13:14:34 +0100

    printone

--- printf.c
+++ printf.c
@@ -1,87 +1,48 @@
-#include "main.h" /* Inclut le fichier d'en-tête principal*/
+#include "main.h"
 #include <stdio.h>
-
-/**
- * print_integer - Print arguments according to a format.
- * @args: arguments
- * Return: The number of characters printed (excluding null byte).
- */
-void print_integer(va_list args)
-{
-	int num, mul;
-
-	mul = 1;
-	num = va_arg(args, int);
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -1 * num;
-	}
-	while (num / mul > 9)
-	{
-		mul = mul * 10;
-	}
-	while (mul != 1)
-	{
-		_putchar ((num / mul) % 10 + '0');
-		mul = mul / 10;
-	}
-	_putchar(num % 10 + '0');
-}
-
-/**
- * print_char_ptr - Print arguments according to a format.
- * @args: arguments
- * Return: The number of characters printed (excluding null byte).
- */
-void print_char_ptr(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	while (*str) /* Parcourt chaque caractère de la chaîne*/
-	{
-		_putchar(*str);
-		str++;
-	}
-}
-
 /**
  * _printf - Print arguments according to a format.
- * @format: format
+ * @format: Format string containing the format specifiers.
  * Return: The number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	const char *ptr;
+	va_list args; /* Déclare une liste pour les arguments variadiques*/
+	int count = 0; /* Initialise le compteur de caractères imprimés à 0*/
+	const char *ptr; /* Déclare un pointeur pour parcourir la chaîne format*/
 
-	va_start(args, format);
+	va_start(args, format); /* Initialise la liste d'arguments variadiques*/
 	for (ptr = format; (*ptr != '\0'); ptr++)
 	{
-		if (*ptr == '%')
+		if (*ptr == '%') /* Vérifie si le caractère actuel est '%'*/
 		{
 			ptr++; /*Passe au caractère suivant*/
-			count++;
-			if (*ptr == 'c')
-				_putchar(va_arg(args, int));
-			else if (*ptr == '%') /*Vérifie si le spécificateur est '%'*/
-				_putchar('%'); /* Imprime le caractère '%'*/
-			else if (*ptr == 's')
-				print_char_ptr(args);
-			else if (*ptr == 'i' || *ptr == 'd')
-				print_integer(args);
-			else
+			if (*ptr == 'c') /* Vérifie si le spécificateur est 'c'*/
 			{
-				_putchar('%');
-				_putchar(*ptr);
+				_putchar(va_arg(args, int));
+				count++; /*Incrémente le compteur*/
+			}
+			else if (*ptr == 's') /* Vérifie si le spécificateur est 's'*/
+			{
+				char *str = va_arg(args, char *);
+
+				while (*str) /* Parcourt chaque caractère de la chaîne*/
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (*ptr == '%')
+			{
+				_putchar('%'); /* Imprime le caractère '%'*/
 			}
 		}
 		else
 		{
 			_putchar(*ptr); /*Imprime le caractère actuel qui n'est pas '%'*/
-			count++; /*Incrémente le compteur*/
 		}
+		count++;
 	}
 	va_end(args);
 	return (count);
