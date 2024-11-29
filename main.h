12f9f0cd30e6b1d5f051e70184365aadb64d5087
hmeyd <yyahmedsalm@gmail.com>
2024-11-29 11:01:07 +0100

    projet ok

--- main.h
+++ main.h
@@ -9,23 +9,9 @@ typedef struct what_format
 	char *type;
 	void (*f)(va_list args);
 }what_format;
-void print_t(const char * const format, ...);
+void print_t(const char *format, ...);
 void print_char(va_list args);
 void print_integer(va_list args);
-void print_porcentage(va_list args);
 void print_char_ptr(va_list args);
 void print_double(va_list args);
-void print_char(va_list args)
-{
-	_putchar(va_arg(args, int)); /* Imprime le caractère passé en argument*/
-}
-void print_char_ptr(va_list args)
-{
-	char *str = va_arg(args, char *); /*Récupère la chaîne de caractères passée en argument*/
-	while (*str) /* Parcourt chaque caractère de la chaîne*/
-	{
-		_putchar(*str); /*Imprime chaque caractère de la chaîne*/
-		str++; /*Passe au caractère suivant de la chaîne*/
-	}
-}
 #endif
